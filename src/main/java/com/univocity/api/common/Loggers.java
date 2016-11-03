fe440e59afb0cc246eb0b0dcf87b7efabe96f85d
Jeronimo Backes <jbax@univocity.com>
2016-11-03 15:03:05 +1030

    adding a Loggers class to centralize logger names

---
+++ src/main/java/com/univocity/api/common/Loggers.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2013 uniVocity Software Pty Ltd. All rights reserved.
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE.txt', which is part of this source code package.
+ */
+
+package com.univocity.api.common;
+
+/**
+ * A central location to provide logger names used internally.
+ *
+ * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
+ */
+public class Loggers {
+
+	/**
+	 * Logger for operations involving network activity.
+	 */
+	public static final String NETWORKING = "NETWORKING";
+
+	/**
+	 * General logger for common operations performed by our software.
+	 */
+	public static final String GENERAL = "GENERAL";
+}
