4420a4b6cd257155fceb6b2215dcb5ab8638c3c6
Jeronimo Backes <jbax@univocity.com>
2017-02-21 18:22:39 +1030

    updating javadocs and handling of fetchResources* methods

--- src/main/java/com/univocity/api/common/StringFilter.java
+++ src/main/java/com/univocity/api/common/StringFilter.java
@@ -1,7 +1,7 @@
 package com.univocity.api.common;
 
 /**
- * Created by adam on 17/02/17.
+ * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
  */
 public interface StringFilter {
 	boolean accept(String str);
