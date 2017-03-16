13cd689db151d7dad6153b2b78f38f44f75ce97c
Adam Bruce <adam@univocity.com>
2017-03-16 11:30:02 +1030

    Updated javadoc for fetchResources related classes.

--- src/main/java/com/univocity/api/common/StringFilter.java
+++ src/main/java/com/univocity/api/common/StringFilter.java
@@ -1,6 +1,8 @@
 package com.univocity.api.common;
 
 /**
+ * A simple interface used to test a given {@link String}
+ *
  * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
  */
 public interface StringFilter {
