5a49fb06004238ddc74fd91a00bcfd2fef023401
Jeronimo Backes <jbax@univocity.com>
2018-02-21 19:28:14 +1030

    switching to markdown to format javadocs.

--- src/main/java/com/univocity/api/common/StringFilter.java
+++ src/main/java/com/univocity/api/common/StringFilter.java
@@ -1,10 +1,18 @@
 package com.univocity.api.common;
 
 /**
- * A simple interface used to test a given {@link String}
+ * A simple interface used to test a given {@code String}
  *
  * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
  */
 public interface StringFilter {
+
+	/**
+	 * Tests whether a given {@code String} conforms to a specific criteria.
+	 *
+	 * @param str the {@code String} to be tested
+	 * @return {@code true} if the given {@code String} conforms to the filtering rules implemented by this interface,
+	 *         otherwise returns {@code false}
+	 */
 	boolean accept(String str);
 }
