e76ee084678030752ba475a8b4b4aaaa00c008f7
Jeronimo Backes <jbax@univocity.com>
2018-07-09 19:04:18 +0930

    Fixed https://github.com/uniVocity/univocity-html-parser-api/issues/2  - default thread pool now spawns deamon threads.

---
+++ src/main/java/com/univocity/api/common/DaemonThreadFactory.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2013 uniVocity Software Pty Ltd. All rights reserved.
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE.txt', which is part of this source code package.
+ */
+
+package com.univocity.api.common;
+
+import java.util.concurrent.*;
+
+/**
+ * A simple thread factory for creating daemon threads
+ */
+public class DaemonThreadFactory implements ThreadFactory {
+	public Thread newThread(Runnable r) {
+		Thread t = new Thread(r);
+		t.setDaemon(true);
+		return t;
+	}
+}
